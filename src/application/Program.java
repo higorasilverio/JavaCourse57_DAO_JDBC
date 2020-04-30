6a69c0675be42a52a265b9351b6100577676de02
higorasilverio <higorasilverio@outlook.com>
2020-04-30 17:24:59 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,8 @@ public class Program {
 		
 		Seller seller = new Seller(21, "Higor", "higor.asilverio_eng@hotmail.com", 
 				new Date(), 3000.00, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 		
